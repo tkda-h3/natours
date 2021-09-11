f36d04b490077b3f69174feaab653e7cfb3cf742
tkda-h3 <18517117+tkda-h3@users.noreply.github.com>
2021-09-11 15:43:30 +0900

    indent

--- src/php/footer.php
+++ src/php/footer.php
@@ -1,23 +1,22 @@
 <footer class="footer">
-  <div class="footer__logo-box">
-<!--            表示する画像をブラウザ幅で制御-->
-      <picture class="footer__logo">
-          <source srcset="<?php echo get_template_directory_uri(); ?>/img/logo-green-small-1x.png 1x, <?php echo get_template_directory_uri(); ?>/img/logo-green-small-2x.png 2x"
-                  media="(max-width: 37.5em)"
-                  src="<?php echo get_template_directory_uri(); ?>/img/logo-green-2x.png">
-          <img srcset="<?php echo get_template_directory_uri(); ?>/img/logo-green-1x.png 1x, <?php echo get_template_directory_uri(); ?>/img/logo-green-2x.png 2x" alt="Full logo" class="footer__logo">
-      </picture>
-  </div>
-  <div class="footer__navigation">
-      <ul class="footer__list">
-          <li class="footer__item"><a href="#" class="footer__link">会社概要</a></li>
-          <li class="footer__item"><a href="#" class="footer__link">お問い合わせ</a></li>
-          <li class="footer__item"><a href="#" class="footer__link">採用</a></li>
-          <li class="footer__item"><a href="#" class="footer__link">プライパシーポリシー</a></li>
-          <li class="footer__item"><a href="#" class="footer__link">利用規約</a></li>
-      </ul>
-  </div>
+    <div class="footer__logo-box">
+        <!--            表示する画像をブラウザ幅で制御-->
+        <picture class="footer__logo">
+            <source srcset="<?php echo get_template_directory_uri(); ?>/img/logo-green-small-1x.png 1x, <?php echo get_template_directory_uri(); ?>/img/logo-green-small-2x.png 2x" media="(max-width: 37.5em)" src="<?php echo get_template_directory_uri(); ?>/img/logo-green-2x.png">
+            <img srcset="<?php echo get_template_directory_uri(); ?>/img/logo-green-1x.png 1x, <?php echo get_template_directory_uri(); ?>/img/logo-green-2x.png 2x" alt="Full logo" class="footer__logo">
+        </picture>
+    </div>
+    <div class="footer__navigation">
+        <ul class="footer__list">
+            <li class="footer__item"><a href="#" class="footer__link">会社概要</a></li>
+            <li class="footer__item"><a href="#" class="footer__link">お問い合わせ</a></li>
+            <li class="footer__item"><a href="#" class="footer__link">採用</a></li>
+            <li class="footer__item"><a href="#" class="footer__link">プライパシーポリシー</a></li>
+            <li class="footer__item"><a href="#" class="footer__link">利用規約</a></li>
+        </ul>
+    </div>
 </footer>
 <?php wp_footer(); ?>
 </body>
+
 </html>
--- src/php/header.php
+++ src/php/header.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        
-        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
-        <?php wp_head(); ?>
-        <link rel="shortcut icon" type="image/png" href="img/favicon.png">
-        
-        <title>Natours | アウトドア好きのための旅行サイト</title>
-    </head>
-    <body>
-    <script>0</script>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+    <?php wp_head(); ?>
+    <link rel="shortcut icon" type="image/png" href="img/favicon.png">
+
+    <title>Natours | アウトドア好きのための旅行サイト</title>
+</head>
+
+<body>
+    <script>
+        0
+    </script>
     <div class="navigation">
         <input type="checkbox" class="navigation__checkbox" id="navi-toggle">
         <label for="navi-toggle" class="navigation__button"><span class="navigation__icon">&nbsp;</span></label>
