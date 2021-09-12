d437f35e1caee1ee4e8d156e68e48a0895c59424
tkda-h3 <18517117+tkda-h3@users.noreply.github.com>
2021-09-12 21:44:51 +0900

    メニューの動的化

--- src/php/footer.php
+++ src/php/footer.php
@@ -3,22 +3,24 @@
     <!--            表示する画像をブラウザ幅で制御-->
     <a href="/">
       <picture class="footer__logo">
-        <source srcset="<?php echo get_template_directory_uri(); ?>/img/logo-green-small-1x.png 1x, <?php echo get_template_directory_uri(); ?>/img/logo-green-small-2x.png 2x" media="(max-width: 37.5em)"
-          src="<?php echo get_template_directory_uri(); ?>/img/logo-green-2x.png">
+        <source srcset="<?php echo get_template_directory_uri(); ?>/img/logo-green-small-1x.png 1x, <?php echo get_template_directory_uri(); ?>/img/logo-green-small-2x.png 2x" media="(max-width: 37.5em)" src="<?php echo get_template_directory_uri(); ?>/img/logo-green-2x.png">
         <img srcset="<?php echo get_template_directory_uri(); ?>/img/logo-green-1x.png 1x, <?php echo get_template_directory_uri(); ?>/img/logo-green-2x.png 2x" alt="Full logo" class="footer__logo">
       </picture>
     </a>
   </div>
+
   <div class="footer__navigation">
-    <ul class="footer__list">
-      <li class="footer__item"><a href="<?php echo site_url('/about'); ?>" class="footer__link">会社概要</a></li>
-      <li class="footer__item"><a href="<?php echo site_url('/contact'); ?>" class="footer__link">お問い合わせ</a></li>
-      <li class="footer__item"><a href="<?php echo site_url('/privacy-policy'); ?>" class="footer__link">プライパシーポリシー</a></li>
-      <li class="footer__item"><a href="<?php echo site_url('/tos'); ?>" class="footer__link">利用規約</a></li>
-    </ul>
+    <?php
+    wp_nav_menu(array(
+      'theme_location' => 'footer_menu',
+      'container' => '',
+      'menu_class' => 'footer__list',
+    ));
+    ?>
+
   </div>
 </footer>
 <?php wp_footer(); ?>
-  </body>
+</body>
 
-  </html>
+</html>
